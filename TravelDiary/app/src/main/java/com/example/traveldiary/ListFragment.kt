37c330993197bd2beb9e7edb6ba1cf85d94b0846
Emine İnan <emineinanx@gmail.com>
2021-01-14 10:40:19 +0300

    Add options menu

--- TravelDiary/app/src/main/java/com/example/traveldiary/ListFragment.kt
+++ TravelDiary/app/src/main/java/com/example/traveldiary/ListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class ListFragment : Fragment() {
 
@@ -20,5 +21,6 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+
     }
 }
--- TravelDiary/app/src/main/java/com/example/traveldiary/MainActivity.kt
+++ TravelDiary/app/src/main/java/com/example/traveldiary/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.traveldiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater=menuInflater
+        menuInflater.inflate(R.menu.travel_add,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.travel_add_item){
+            val action=ListFragmentDirections.actionListFragmentToTravelFragment()
+            Navigation.findNavController(this,R.id.fragment).navigate(action)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
