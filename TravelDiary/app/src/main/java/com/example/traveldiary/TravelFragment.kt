b0adecf1eaa07b9a8ae6cbdb0eb992e8539f9cd6
Emine İnan <emineinanx@gmail.com>
2021-01-15 11:15:50 +0300

    Request user permission to select images

--- TravelDiary/app/src/main/java/com/example/traveldiary/TravelFragment.kt
+++ TravelDiary/app/src/main/java/com/example/traveldiary/TravelFragment.kt
@@ -1,10 +1,16 @@
 package com.example.traveldiary
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.fragment_travel.*
+import java.util.jar.Manifest
 
 
 class TravelFragment : Fragment() {
@@ -22,4 +28,33 @@ class TravelFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_travel, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonSave.setOnClickListener {
+            save(it)
+        }
+        imageView.setOnClickListener{
+            selectImage(it)
+        }
+        
+    }
+
+    fun save(view: View){
+
+    }
+
+    fun selectImage(view: View){
+        activity?.let{
+            if(ContextCompat.checkSelfPermission(it.applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+            }else{
+                val galIntent=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galIntent,2)
+            }
+        }
+
+    }
+
+
 }
