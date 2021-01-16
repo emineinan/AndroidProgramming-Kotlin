91c70a68c2a2e8645aa998014fa23dce73cf4d32
Emine İnan <emineinanx@gmail.com>
2021-01-16 10:39:25 +0300

    Add image operations

--- TravelDiary/app/src/main/java/com/example/traveldiary/TravelFragment.kt
+++ TravelDiary/app/src/main/java/com/example/traveldiary/TravelFragment.kt
@@ -1,7 +1,12 @@
 package com.example.traveldiary
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
@@ -14,8 +19,8 @@ import java.util.jar.Manifest
 
 
 class TravelFragment : Fragment() {
-
-
+    var selectedİmg: Uri?=null
+    var selectedBitmp: Bitmap?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -46,9 +51,9 @@ class TravelFragment : Fragment() {
 
     fun selectImage(view: View){
         activity?.let{
-            if(ContextCompat.checkSelfPermission(it.applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(it.applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){ //not permissioned
                 requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
-            }else{
+            }else{ //permission already granted
                 val galIntent=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 startActivityForResult(galIntent,2)
             }
@@ -56,5 +61,54 @@ class TravelFragment : Fragment() {
 
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode==1){
+            if(grantResults.size>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){//we got permission
+                val galIntent=Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galIntent,2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode==2&&resultCode==Activity.RESULT_OK&&data!=null){
+            selectedİmg=data.data
+            try {
+                context?.let {
+                    if(selectedİmg!=null){
+                        if(Build.VERSION.SDK_INT>=28){
+                            val source=ImageDecoder.createSource(it.contentResolver,selectedİmg!!)
+                            selectedBitmp=ImageDecoder.decodeBitmap(source)
+                            imageView.setImageBitmap(selectedBitmp)
+                        }
+                    }else{
+                        selectedBitmp=MediaStore.Images.Media.getBitmap(it.contentResolver,selectedİmg)
+                        imageView.setImageBitmap(selectedBitmp)
+                    }
+                }
+
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    fun reduceImageSize(selectedBitmp:Bitmap,maxSize:Int):Bitmap{
+        var width=selectedBitmp.width
+        var height=selectedBitmp.height
+        val bitmapRate:Double=width.toDouble()/height.toDouble()
+        if(bitmapRate>1){ //horizontal image
+            width=maxSize
+            val newHeight=width/bitmapRate
+            height=newHeight.toInt()
+        }else{ //vertical image
+            height=maxSize
+            val newWidth=height*bitmapRate
+            width=newWidth.toInt()
+        }
+
+        return Bitmap.createScaledBitmap(selectedBitmp,width,height,true)
+    }
 }
