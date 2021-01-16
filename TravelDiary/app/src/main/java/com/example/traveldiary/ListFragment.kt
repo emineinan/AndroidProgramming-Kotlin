62f399a8e812a8c9cc306e37a70a8608864ff8f2
Emine İnan <emineinanx@gmail.com>
2021-01-16 20:27:54 +0300

    Receive data

--- TravelDiary/app/src/main/java/com/example/traveldiary/ListFragment.kt
+++ TravelDiary/app/src/main/java/com/example/traveldiary/ListFragment.kt
@@ -1,5 +1,6 @@
 package com.example.traveldiary
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,8 @@ import androidx.navigation.fragment.findNavController
 
 class ListFragment : Fragment() {
 
+    var cityNameList=ArrayList<String>()
+    var cityIdList=ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +25,33 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        sqlGetData()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun sqlGetData(){
+        try {
+            activity?.let {
+                val database=it.openOrCreateDatabase("Travels", Context.MODE_PRIVATE,null)
+                val cursor=database.rawQuery("SELECT * FROM travels",null)
+                val cityNameIndex=cursor.getColumnIndex("cityName")
+                val cityIdIndex=cursor.getColumnIndex("id")
+
+                cityIdList.clear()
+                cityNameList.clear()
+
+                while (cursor.moveToNext()){
+                    cityNameList.add(cursor.getString(cityNameIndex))
+                    cityIdList.add(cursor.getInt(cityIdIndex))
+                }
+                cursor.close()
+            }
+
+        }catch (e:Exception){
+
+        }
     }
 }
