20be0fb63abebfc969c1909fbb9c05283ae229d3
Emine İnan <emineinanx@gmail.com>
2021-02-02 16:25:51 +0300

    Find the current location

--- GoogleMapsApp/app/src/main/java/com/example/googlemapsapp/MapsActivity.kt
+++ GoogleMapsApp/app/src/main/java/com/example/googlemapsapp/MapsActivity.kt
@@ -8,6 +8,7 @@ import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -35,22 +36,50 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        /*val ankara = LatLng(39.9208372, 32.8454561) //Latidude,Longitude
-        mMap.addMarker(MarkerOptions().position(ankara).title("Marker in Ankara"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ankara,15f))*/
 
-        locationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        locationListener=object:LocationListener{
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = object : LocationListener {
             override fun onLocationChanged(location: Location) {
-                TODO("Not yet implemented")
+                mMap.clear()
+                val currentLocation = LatLng(location.latitude, location.longitude) //Latidude,Longitude
+                mMap.addMarker(MarkerOptions().position(currentLocation).title("Marker in Current Location"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15f))
             }
 
         }
 
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){ //if not allowed
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) { //if not allowed
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
 
-        }else{ //if allowed
+        } else { //if allowed
 
+        }
+
+        fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+        ) {
+            if (requestCode == 1) {
+                if (grantResults.size > 0) {
+                    locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        1,
+                        1f,
+                        locationListener
+                    )
+                }
+            }
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         }
     }
 }
