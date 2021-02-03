9d5bc4cd276759e5243dba2588369bd3b9c79574
Emine İnan <emineinanx@gmail.com>
2021-02-03 18:24:15 +0300

    Add GoogleMapsApp

--- GoogleMapsApp/app/src/main/java/com/example/googlemapsapp/MapsActivity.kt
+++ GoogleMapsApp/app/src/main/java/com/example/googlemapsapp/MapsActivity.kt
@@ -23,79 +23,93 @@ import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+
     private lateinit var mMap: GoogleMap
-    private lateinit var locationManager: LocationManager
-    private lateinit var locationListener: LocationListener
+    private lateinit var locationManager : LocationManager
+    private lateinit var locationListener : LocationListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapLongClickListener(listenerObj)
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        locationListener = object : LocationListener {
-            override fun onLocationChanged(location: Location) {
-                mMap.clear()
-                val currentLocation = LatLng(location.latitude, location.longitude) //Latidude,Longitude
-                mMap.addMarker(MarkerOptions().position(currentLocation).title("Marker in Current Location"))
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15f))
 
-                val geocoder=Geocoder(this@MapsActivity, Locale.getDefault())
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(p0: Location) {
+                mMap.clear()
+                val myCurrentLocation = LatLng(p0.latitude,p0.longitude)
+                mMap.addMarker(MarkerOptions().position(myCurrentLocation).title("Current Location"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myCurrentLocation,15f))
+
+                val geocoder = Geocoder(this@MapsActivity, Locale.getDefault())
+
                 try {
-                    val addressList=geocoder.getFromLocation(location.latitude,location.longitude,1)
-                    if(addressList.size>0){
+                    val addressList = geocoder.getFromLocation(p0.latitude,p0.longitude,1)
+                    if(addressList.size > 0) {
                         println(addressList.get(0).toString())
                     }
-                }catch (e:Exception){
+                } catch (e: Exception){
                     e.printStackTrace()
                 }
             }
-
         }
 
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) { //if not allowed
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                1
-            )
-
-        } else { //if allowed
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),1)
+        } else {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,1f,locationListener)
-            var lastLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-            if(lastLocation!=null){
-                val lastKnwnLatlang=LatLng(lastLocation.latitude,lastLocation.longitude)
-                mMap.addMarker(MarkerOptions().position(lastKnwnLatlang).title("Marker in Current Location"))
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastKnwnLatlang,15f))
+            val myLastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (myLastLocation != null) {
+                val myLastLatLng = LatLng(myLastLocation.latitude,myLastLocation.longitude)
+                mMap.addMarker(MarkerOptions().position(myLastLatLng).title("Last Location"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLastLatLng,15f))
             }
         }
+    }
 
-        fun onRequestPermissionsResult(
+    override fun onRequestPermissionsResult(
             requestCode: Int,
             permissions: Array<out String>,
             grantResults: IntArray
-        ) {
-            if (requestCode == 1) {
-                if (grantResults.size > 0) {
-                    locationManager.requestLocationUpdates(
-                        LocationManager.GPS_PROVIDER,
-                        1,
-                        1f,
-                        locationListener
-                    )
+    ) {
+
+        if (requestCode == 1){
+            if (grantResults.size > 0){
+                if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1,1f,locationListener)
                 }
             }
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    val listenerObj = object : GoogleMap.OnMapLongClickListener {
+        override fun onMapLongClick(p0: LatLng?) {
+            mMap.clear()
+            val geocoder = Geocoder(this@MapsActivity,Locale.getDefault())
+            if(p0 != null) {
+                var address = ""
+                try {
+                    val adresListesi = geocoder.getFromLocation(p0.latitude,p0.longitude,1)
+                    if (adresListesi.size > 0 ) {
+                        if (adresListesi.get(0).thoroughfare != null) {
+                            address += adresListesi.get(0).thoroughfare
+                            if(adresListesi.get(0).subThoroughfare != null) {
+                                address += adresListesi.get(0).subThoroughfare
+                            }
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+                mMap.addMarker(MarkerOptions().position(p0).title(address))
+            }
         }
     }
 }
