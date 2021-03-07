4460b41abdf31773b45997c482d520ce0ab7523f
Emine İnan <emineinanx@gmail.com>
2021-03-07 19:50:59 +0300

    Create db folder

---
+++ ToDoList/app/src/main/java/com/example/todolist/db/TaskRepository.kt
@@ -0,0 +1,21 @@
+package com.example.todolist.db
+
+import android.content.Context
+
+class TaskRepository(var context: Context){
+    fun getAllTask(){
+
+    }
+
+    fun insertTask(){
+
+    }
+
+    fun deleteTask(){
+
+    }
+
+    fun updateTask(){
+
+    }
+}
