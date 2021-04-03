6ba5f78c708fcbd28afc86ba775a4260fa266fef
Emine İnan <emineinanx@gmail.com>
2021-04-03 10:50:50 +0300

    Add splash screen using Handler

---
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            splashConstraintLayout.visibility= View.GONE
+        }, 3000)
+    }
+}
